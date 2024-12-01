cb7a59eacebdafd57c3dd064cb7f66a463306c0e
TJ Aditya <adityatj.me@gmail.com>
2024-12-02 00:30:17 +0530

    Update req.js

--- req.js
+++ req.js
@@ -6,12 +6,12 @@
     "https://fujifilmxindia.com/wp-json/wc/store/products/34439"
   );
 
-  blackPromise.then(async unction(response) {
+  blackPromise.then(async function(response) {
     let $prod = document.getElementById('prod');
     let json = await response.json();
     $prod.innerText += `\n\nBlack: ${json.is_in_stock}\n\n`;
   });
-  silverPromise.then((response) => {
+  silverPromise.then(async function(response) {
     let $prod = document.getElementById('prod');
     let json = await response.json();
     $prod.innerText += `\n\nSilver: ${json.is_in_stock}\n\n`;

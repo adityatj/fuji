71eef16805b5669ccaa9c7aef58f4aa26366f900
TJ Aditya <adityatj.me@gmail.com>
2024-12-02 00:22:26 +0530

    Update req.js

--- req.js
+++ req.js
@@ -1,16 +1,17 @@
 (function() {
-  let $prod = document.getElementById('prod');
   const blackPromise = fetch(
-    "https://fujifilmxindia.com/wp-json/wc/store/products/34439", { mode: "no-cors" }
+    "https://fujifilmxindia.com/wp-json/wc/store/products/34439"
   );
   const silverPromise = fetch(
-    "https://fujifilmxindia.com/wp-json/wc/store/products/34439", { mode: "no-cors" }
+    "https://fujifilmxindia.com/wp-json/wc/store/products/34439"
   );
 
   blackPromise.then((response) => {
+    let $prod = document.getElementById('prod');
     $prod.innerText += `\n\nBlack: ${response.data.is_in_stock}\n\n`;
   });
   silverPromise.then((response) => {
+    let $prod = document.getElementById('prod');
     $prod.innerText += `\n\nSilver: ${response.data.is_in_stock}\n\n`;
   });
 
